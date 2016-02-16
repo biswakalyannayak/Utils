e2b6be4545099d77b6a8805c69c3ef92a5ebc3b8
biswakalyannayak <nayak.biswakalyan@yahoo.com>
2016-02-16 10:19:40 +0530

    Modified with new logic

--- Utility/src/main/java/com/nc/utility/FileAndaParser.java
+++ Utility/src/main/java/com/nc/utility/FileAndaParser.java
@@ -6,7 +6,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -16,55 +19,91 @@ import java.util.regex.Pattern;
 public class FileAndaParser {
 	
 	static Matcher headerMatcher = Pattern.compile("Serialnmber\\|Sortorder\\|PartNumber\\|Partname\\|level1\\|serialized\\|Source\\|Lastupdateddate").matcher("");
-	static Matcher dataMatcher = Pattern.compile("8TK00204|-99101725|1061657|WIRING GP-REAR|10732256C76004E8467||E|30-AUG-12|").matcher("");
 	
 	static String newFileHeader = "Serialnmber|Sortorder|PartNumber|Partname|level1|serialized|Source|Lastupdateddate|Indent";
 	static List<String> newFileContent = new ArrayList<String>();
 	static Set<String> newContentwithoutLevel1 = new TreeSet<String>();
 	static String [] headerArr = null;
+	static TreeSet<String> parentSet = new TreeSet<String>();
+	static TreeSet<String> dataSet = new TreeSet<String>();
+	
 	
 	
 	public static void main(String[] args) {
 		boolean headerFound = false;
 		
 		TreeMap<String,String> parentMap = new TreeMap<String, String>();
-		String fileName="C:\\Users\\biswnaya\\git\\Utils\\Utility\\src\\main\\java\\com\\nc\\utility\\Final1.csv";
-		String outputFile="C:\\Users\\biswnaya\\git\\Utils\\Utility\\src\\main\\java\\com\\nc\\utility\\Result1.csv";
-		
+		//String fileName="C:\\Users\\biswnaya\\git\\Utils\\Utility\\src\\main\\java\\com\\nc\\utility\\Final1.csv";
+		//String outputFile="C:\\Users\\biswnaya\\git\\Utils\\Utility\\src\\main\\java\\com\\nc\\utility\\Result1.csv";
+		String fileName="E:\\WorkSpace\\CommonUtility\\GitHub\\Utility\\src\\main\\java\\com\\nc\\utility\\Final1.csv";
+		String outputFile="E:\\WorkSpace\\CommonUtility\\GitHub\\Utility\\src\\main\\java\\com\\nc\\utility\\Result1.csv";
+				
 		 try{
 
 	          headerFound  = processForMetaData(parentMap, fileName);
 	          newFileContent.add(newFileHeader);
-	          //Map with lines which has not Level1 entry
-	          Set<String> parentSet =  new TreeSet<String>(parentMap.keySet());
-	          for (String parent : parentSet) {
-	        	  String parentLine = parentMap.get(parent);
-	        	  String [] dataArr = parentLine.split("\\|");
-	        	  if(getIndexOf(dataArr, parent) == getIndexOf(headerArr,"PartNumber")){
-	        		  parentLine = parentLine+"0";
-	        		  int levalIndx = getIndexOf(headerArr, "level1");
-	        		  String[] parentDataArr = parentLine.split("\\|");
-	        		  if(parentDataArr[levalIndx] ==null || parentDataArr[levalIndx].isEmpty()){
-	        			  newContentwithoutLevel1.add(parentLine);
-	        		  }else{
-	        			  newFileContent.add(parentLine);
-	        		  }
-	        		  //Remove the parent from map for further processing
-	        		  parentMap.remove(parent);
-	        		  for (String parentMapKey : parentMap.keySet()) {
-	        				  String line = parentMap.get(parentMapKey);
-	        				  String[] chidDataArr = line.split("\\|");
-	        				  
-	        				  String childV = chidDataArr[levalIndx];
-	        				  if(childV.contains(parent)){
-	        					  line = line+"1";
-	        					  newFileContent.add(line);
-	        				  }
-					  }
-	        	  }else{
-	        		 System.err.println("Skiping the line as this is not in format. Line:"+parentLine);
-	        	  }
-	          }
+	          int levalIndx = getIndexOf(headerArr, "level1");
+    		  int serilizeIndx = getIndexOf(headerArr, "serialized");
+    		  int partNumberIndx = getIndexOf(headerArr,"PartNumber");
+    		  
+    		  System.err.println(parentSet);
+    		  System.out.println(dataSet);
+    		  
+	          for (String parentLine : parentSet) {
+	        	  String [] parentDataArr = parentLine.split("\\|");
+	        	  parentLine = parentLine+"0";
+	        	  /*if((parentDataArr[levalIndx] ==null || parentDataArr[levalIndx].isEmpty())
+        				  && (parentDataArr[serilizeIndx] ==null || parentDataArr[serilizeIndx].isEmpty())){
+        			  newContentwithoutLevel1.add(parentLine);
+        		  }else if((parentDataArr[levalIndx] ==null || parentDataArr[levalIndx].isEmpty())){*/
+        			  newFileContent.add(parentLine);
+        			  String parentcode = parentDataArr[partNumberIndx];
+        			  Iterator<String> dataLineIt1 = dataSet.iterator();
+        			  while (dataLineIt1.hasNext()) {
+        				String dataLine = dataLineIt1.next();
+        				String[] dataArr = dataLine.split("\\|");
+        				String level1 = dataArr[levalIndx];
+        				String childParentCode = dataArr[partNumberIndx];
+        				if(level1.equalsIgnoreCase(parentcode)){
+        					
+        					dataLine = dataLine+"1";
+        					newFileContent.add(dataLine);
+        					dataLineIt1.remove();
+        					dataSet.remove(dataLine);
+        					extractParent((TreeSet<String>) dataSet.clone(),childParentCode,levalIndx,partNumberIndx);
+        				}
+        				else if(level1.contains(parentcode)){
+        					String subChildParentCode;
+        					if(level1.length()>parentcode.length()+6){
+        						subChildParentCode = level1.substring(parentcode.split("|").length,parentcode.split("|").length+6);
+        					}
+        					else{
+        						subChildParentCode = level1.substring((parentcode).split("|").length);
+        					}
+        					TreeSet<String > temp = (TreeSet<String>) dataSet.clone();
+        					Iterator<String> tempItr = temp.iterator();
+              			  	while (tempItr.hasNext()) {
+	              			  	String tempDataLine = tempItr.next();
+	            				String[] tempDataArr = dataLine.split("\\|");
+	            				String tempLevel1 = dataArr[levalIndx];
+	            				String tempChildParentCode = dataArr[partNumberIndx];
+	            				if(tempLevel1.equalsIgnoreCase(subChildParentCode)){
+	            					tempDataLine = tempDataLine+"1";
+		            				tempItr.remove();
+		            				temp.remove(tempDataLine);
+		        					newFileContent.add(tempDataLine);
+	            				}else if(tempLevel1.contains(subChildParentCode)){
+	            					String subChildParentCode1 = level1.substring((parentcode+subChildParentCode).split("|").length);
+	            					newFileContent.add(tempDataLine);
+	            				}
+	            				
+              			  	}
+        					
+        				}
+					}
+        			  
+        			 
+			}
 	          
 	          System.out.println("Size"+parentMap.size()+parentMap);
 	          System.err.println("Size"+newFileContent.size()+newFileContent);
@@ -84,6 +123,41 @@ public class FileAndaParser {
 	          System.out.println("Error while reading file line by line:" + e.getMessage());                      
 	       }
 		
+	}
+
+	private static void extractParent(TreeSet<String> dataSet1, String childParentCode, int levalIndx, int partNumberIndx) {
+		  Iterator<String> dataLineIt1 = dataSet1.iterator();
+		  while (dataLineIt1.hasNext()) {
+			String dataLine = dataLineIt1.next();
+			String[] dataArr = dataLine.split("\\|");
+			String level1 = dataArr[levalIndx];
+			String chparentcode = dataArr[partNumberIndx];
+			if(level1.equalsIgnoreCase(childParentCode)){
+				dataSet1.remove(dataLine);
+				dataLine = dataLine+"1";
+				dataSet1.remove(dataLine);
+				newFileContent.add(dataLine);
+				dataLineIt1.remove();
+				exrtractRelativeParent((TreeSet<String>) dataSet1.clone(),childParentCode,levalIndx,partNumberIndx);
+			}
+		}
+		
+	}
+
+	private static void exrtractRelativeParent(TreeSet<String> dataSet2, String childParentCode, int levalIndx, int partNumberIndx) {
+		 Iterator<String> dataLineIt2 = dataSet2.iterator();
+		  while (dataLineIt2.hasNext()) {
+			String dataLine = dataLineIt2.next();
+			String[] dataArr = dataLine.split("\\|");
+			String level1 = dataArr[levalIndx];
+			if(level1.contains(childParentCode)){
+				dataLine = dataLine+"1";
+				newFileContent.add(dataLine);
+				dataLineIt2.remove();
+				dataSet2.remove(dataLine);
+			}
+		  
+	  }
 	}
 
 	private static boolean processForMetaData(TreeMap<String,String> parentMap,
@@ -111,7 +185,12 @@ public class FileAndaParser {
 			  if(flag){
 				  dataArr = line.split("\\|");
 				  if(dataArr.length==headerArr.length){
-					  
+					  int levalIndx = getIndexOf(headerArr, "level1");
+					  if((dataArr[levalIndx] ==null || dataArr[levalIndx].isEmpty())){
+	        			  parentSet.add(line);
+	        		  }else{
+	        			  dataSet.add(line);
+	        		  }
 					  //generate map which has level1
 					  int parentIndx = getIndexOf(headerArr,"PartNumber");
 					  parentMap.put(dataArr[parentIndx],line);
